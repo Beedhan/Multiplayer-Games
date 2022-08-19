9f4f09a6ba6832599ef21ab7c36b019cac066adc
Beedhan <45665443+Beedhan@users.noreply.github.com>
2022-08-19 22:43:19 +0545

    cors added

--- client/utils/socket.ts
+++ client/utils/socket.ts
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "https://adf4-2400-1a00-b1e0-23e1-29de-b70-5e1b-2ae7.in.ngrok.io"
-);
+export const socket = io("multiplayer.pasaale.xyz");
